ffa9d0424e15cfb44598f1ea96c2d18528204630
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-05-09 17:18:21 +0700

    add file

--- Lab01/app/src/main/java/com/example/lab01/MainActivity.java
+++ Lab01/app/src/main/java/com/example/lab01/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lab01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.lab01.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTong;
+    EditText editA, editB, editKq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editA = findViewById(R.id.editA);
+        editB = findViewById(R.id.editB);
+        editKq = findViewById(R.id.editKq);
+        btnTong = findViewById(R.id.btnTong);
+
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                editKq.setText((a+b) + "");
+            }
+        });
+
     }
 }
