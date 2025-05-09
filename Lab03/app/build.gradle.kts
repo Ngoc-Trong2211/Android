c37773b8107f1b9d667d42d7cd80faf04a839a2a
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-05-09 18:53:27 +0700

    add file

--- Lab03/app/build.gradle.kts
+++ Lab03/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.lab03"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lab03"
--- Lab03/app/src/main/java/com/example/lab03/MainActivity.java
+++ Lab03/app/src/main/java/com/example/lab03/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.lab03;
 
+import static java.lang.Math.abs;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
+    EditText editA, editB, editKq;
+    Button btnTong, btnHieu, btnTich, btnThuong;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,54 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        editA = findViewById(R.id.editA);
+        editB = findViewById(R.id.editB);
+        editKq = findViewById(R.id.editKq);
+        btnTong = findViewById(R.id.btnTong);
+        btnHieu = findViewById(R.id.btnHieu);
+        btnTich = findViewById(R.id.btnTich);
+        btnThuong = findViewById(R.id.btnThuong);
+
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                editKq.setText((a+b) +"");
+            }
+        });
+
+        btnHieu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                editKq.setText(a-b +"");
+            }
+        });
+
+        btnTich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                editKq.setText((long)(a*b) +"");
+            }
+        });
+
+        btnThuong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat decimalFormat = new DecimalFormat("#.##");
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                if (b==0){
+                    Toast.makeText(MainActivity.this, "Loi vi mau bang 0!", Toast.LENGTH_SHORT).show();
+                }
+                else  editKq.setText(decimalFormat.format((double) a/b));
+            }
         });
     }
 }
