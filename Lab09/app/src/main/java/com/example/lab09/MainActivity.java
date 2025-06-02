d209b671dfe72ec3fc386bc1531160d20520620f
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-06-02 14:47:50 +0700

    add file

---
+++ Lab09/app/src/main/java/com/example/lab09/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.lab09;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton, imageButton1;
+    Boolean flag = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imageButton = findViewById(R.id.imageButton);
+        imageButton1 = findViewById(R.id.imageButton2);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent);
+                if (flag == true){
+                    imageButton.setImageResource(R.drawable.play);
+                    flag = false;
+                }
+                else{
+                    imageButton1.setImageResource(R.drawable.stop);
+                    flag = true;
+                }
+            }
+        });
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                stopService(intent);
+                imageButton1.setImageResource(R.drawable.pause);
+                flag = true;
+            }
+        });
+    }
+}
